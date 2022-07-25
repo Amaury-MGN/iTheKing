d48ef588211c1a292545af7c3cc30a87b332ed29
Amaury <amaury.meugnier@hotmail.fr>
2022-07-25 18:11:35 +0200

    auth_token

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\authController;
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Laravel\Sanctum\Sanctum;
@@ -16,10 +16,15 @@ use Laravel\Sanctum\Sanctum;
 |
 */
 
-Route::post('login', [authController::class, 'authenticate' ]);
-Route::get('dashboard', [authController::class, 'dashboard'])
-->middleware('auth:sanctum');
+Route::post('login', [AuthController::class, 'authenticate'])
+->name('authenticate');
 
+Route::post('privateInformations', function () {
+    return response()->json([
+        'information' => 'test response = true'
+    ]);
+})
+->middleware('auth:sanctum');
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
